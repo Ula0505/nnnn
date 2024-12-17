4f932cd04e7651e4dcffebbbb41fe3f8689641bc
Duenkova Ulyana <ulana.duenkova@gmail.com>
2024-12-17 14:14:55 +0300

    Сумма и разность

--- main.cpp
+++ main.cpp
@@ -6,5 +6,7 @@ int main(){
 	int a, b;
 	std::cout << "Введите a и b: ";
 	std::cin >> a >> b;
+	std::cout << "a + b = " << a+b << std::endl;
+	std::cout << "a + b = " << a+b << std::endl;
 	return 0;
 }
